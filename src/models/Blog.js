d63f8ac09234dd1f42fa57a3dde4b73b60e0ce7d
alexmquan <alexmquan93@gmail.com>
2023-03-30 14:14:00 -0600

    model build out complete

---
+++ src/models/Blog.js
@@ -0,0 +1,15 @@
+import { Profile } from "./Account.js"
+
+export class Blog {
+  constructor(data) {
+    this.id = data.id
+    this.title = data.title
+    this.body = data.body
+    this.imgUrl = data.imgUrl
+    this.tags = data.tags
+    this.published = data.published
+    this.creatorId = data.creatorId
+    this.createdAt = new Date(data.createdAt)
+    this.creator = new Profile(data.creator)
+  }
+}
